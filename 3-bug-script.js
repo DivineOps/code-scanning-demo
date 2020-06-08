c348534f25b2f2f10414f37147542983185c5811
Sasha Rosenbaum <divineops@github.com>
2020-06-08 10:38:36 -0500

    Create 3-bug-script.js

---
+++ 3-bug-script.js
@@ -0,0 +1,28 @@
+const apiKey = "12345678901234567890123456789012-12345678-12345678";
+const db = "abc123.database.secure.windows.net"
+
+// define database and app
+const pg = require('pg');
+const app = require('express')();
+const pool = new pg.Pool();
+ 
+// Get a category from the database
+app.get("/category/:category", function (req, res) {
+  
+  // BAD: the category might have SQL special characters in it
+  var query1 = "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='"
+             + req.params.category + 
+             "' ORDER BY PRICE";
+
+  pool.query(query1, [], function(err, results) {
+    // process results
+  });
+ 
+ res.send('Results')
+
+});
+
+// shortened endsWith function by one line of code
+function endsWith(x, y) {
+  return x.lastIndexOf(y) === x.length - y.length;
+}
